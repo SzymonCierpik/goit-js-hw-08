13cca6d9e0e02bf8bb43d3e28f52f9d92d6440ea
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-03 14:08:34 +0100

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,39 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+function buildGallery(items) {
+  const galleryElement = document.querySelector(".gallery");
+  const galleryMarkup = items
+    .map(({ preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+        </a>
+      </div>
+    `;
+    })
+
+    .join("");
+
+  galleryElement.innerHTML = galleryMarkup;
+}
+
+buildGallery(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const instance = basicLightbox
+    .create(
+      `
+		<img width="1400" height="900" src="${e.target.getAttribute("data-source")}">
+	`
+    )
+    .show();
+});
