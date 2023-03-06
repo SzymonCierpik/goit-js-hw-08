5360b7ffcaff0959aa531b0e72177bfac2e88ccc
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-06 16:54:30 +0100

    aktualizacja

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+const seconds = localStorage.getItem(`videoplayer-current-time`) || 0;
+
+player.setCurrentTime(seconds);
+player.on('timeupdate', throttle(onPlay, 1000));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
 
+const saveFormStateToLocalStorage = () => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+};
+
+const loadFormStateFromLocalStorage = () => {
+  const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
+  if (formState) {
+    emailInput.value = formState.email || '';
+    messageInput.value = formState.message || '';
+  }
+};
+
+const resetFormState = () => {
+  localStorage.removeItem('feedback-form-state');
+  emailInput.value = '';
+  messageInput.value = '';
+};
+
+const saveFormStateToLocalStorageThrottled = _.throttle(
+  saveFormStateToLocalStorage,
+  500
+);
+emailInput.addEventListener('input', saveFormStateToLocalStorageThrottled);
+messageInput.addEventListener('input', saveFormStateToLocalStorageThrottled);
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  console.log('Form submitted:', {
+    email: emailInput.value,
+    message: messageInput.value,
+  });
+});
