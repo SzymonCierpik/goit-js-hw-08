7f94f3655e2eee6ffc16b0d978dff2494653888c
Szymon Cierpik <109699547+SzymonCierpik@users.noreply.github.com>
2023-03-06 18:43:46 +0100

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,26 +1,26 @@
 import throttle from 'lodash.throttle';
-​
+
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
-​
+
 const formState = JSON.parse(localStorage.getItem('feedback-form-state'));
 if (formState) {
   emailInput.value = formState.email;
   messageInput.value = formState.message;
 }
-​
+
 const onFormInput = () => {
   const object = {
     email: emailInput.value.trim(),
     message: messageInput.value.trim(),
   };
-​
+
   localStorage.setItem('feedback-form-state', JSON.stringify(object));
 };
-​
+
 form.addEventListener('input', throttle(onFormInput, 500));
-​
+
 form.addEventListener('submit', event => {
   event.preventDefault();
   console.log('Form submitted:', {
